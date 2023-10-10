29818e1ec347e4d50f6b7f89ffb1e38d87f39c31
Cicero Silva <cj.silva@totvs.com.br>
2023-10-10 12:19:18 -0300

    Respostas

--- challenge-02/README.md
+++ challenge-02/README.md
@@ -4,29 +4,34 @@ Nesse exercício, você está livre para escolher os nomes para suas variáveis 
 
 ```js
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-?
+function soma(a, b {
+  return a + b
+})
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-?
+var soma = soma(10 + 5) + 5
 
 // Qual o valor atualizado dessa variável?
-?
+15
 
 // Declare uma nova variável, sem valor.
-?
+var x
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-?
+function mensagem () {
+  x = 20
+  return "O valor da variável agora é " + x
+}
 
 // Invoque a função criada acima.
-?
+mensagem()
 
 // Qual o retorno da função? (Use comentários de bloco).
-?
+O valor da variável agora é 20
 
 /*
 Crie uma função com as seguintes características:
@@ -35,19 +40,25 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-?
+function myfunction(a,b,c){
+  if( !(a || b || c) ){
+    return "Preencha todos os valores corretamente!"
+  }else{
+    return a * b * c + 2
+  }
+}
 
 // Invoque a função criada acima, passando só dois números como argumento.
-?
+myfunction(5,5)
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+Preencha todos os valores corretamente!
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-?
+myfunction(5,5,5)
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+125
 
 /*
 Crie uma função com as seguintes características:
@@ -58,7 +69,12 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-?
+function (a,b,c){
+    argumentos += a?1:0
+
+
+
+}
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
 ?
--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,47 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+sum = function calculateSum(a, b) {
+  return a + b;
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+var a = 5;
+var b = 8;
+result = sum(a, b);
+console.log(`A soma de ${a} e ${b} é igual a ${result}.`);
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log(`O nome da função que faz a soma é ${sum.name}.`);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName() {
+  return "Cícero";
+}
+
+console.log(showName());
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log(`A função ${varShowName.name} retorna ${varShowName()}.`);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,28 +57,64 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+function calculator(operacao) {
+  return function (a, b) {
+    var result = 0;
+    switch (operacao) {
+      case "+":
+        result = a + b;
+        break;
+      case "-":
+        result = a - b;
+        break;
+      case "*":
+        result = a * b;
+        break;
+      case "/":
+        result = a / b;
+        break;
+      case "%":
+        result = a % b;
+        break;
+      default:
+        return "Operação inválida.";
+    }
+    return `Resultado da operação: ${a} ${operacao} ${b} = ${result}.`;
+  };
+}
 
+console.log(calculator("+")(10, 5));
+console.log(calculator("-")(10, 5));
+console.log(calculator("*")(10, 5));
+console.log(calculator("/")(10, 5));
+console.log(calculator()(10, 5));
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-// ?
+var sum = calculator("+");
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-// ?
+console.log(sum(20, 10));
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+var subtraction = calculator("-");
+var multiplication = calculator("*");
+var division = calculator("/");
+var mod = calculator("%");
+
+console.log(subtraction(20, 10));
+console.log(multiplication(20, 10));
+console.log(division(20, 10));
+console.log(mod(20, 10));
 
 /*
-Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
-no console.
+Faça uma operação com cada uma das funções criadas acima, mostrando o resultado no console.
 */
 // ?
